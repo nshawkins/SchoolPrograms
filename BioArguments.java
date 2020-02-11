b99fb4f793dbb49b3589a0f1c83654b2cea6ccc0
nshawkins <49796257+nshawkins@users.noreply.github.com>
2020-02-10 20:37:36 -0600

    Delete BioArguments.java

--- BioArguments.java
+++
@@ -1,8 +0,0 @@
-public class BioArguments{
-  public static void main(String[] args){
-    System.out.println("First name:\t" + args[0]);
-    System.out.println("Last name:\t" + args[1]);
-    System.out.println("DOB:\t\t" + args[2]);
-    System.out.println("Major:\t\t" + args[3]);
-  }
-}
