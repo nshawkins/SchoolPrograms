bc5584b76e086d70cafd9a978c9917b062fbef5b
nshawkins <49796257+nshawkins@users.noreply.github.com>
2020-02-10 20:41:51 -0600

    Delete Image.java

--- Image.java
+++
@@ -1,9 +0,0 @@
-public abstract class Image {
-    protected String magic_number;
-    protected int width;
-    protected int height;
-    protected int depth;
-
-    abstract void Rotate90(); //rotate 90 degrees clockwise
-    abstract void FlipVertical(); 
-}
